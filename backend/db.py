2d5a035ec59fca8403bc5246e573fe35845e730c
João Pedro <jpedro1580@gmail.com>
2022-07-29 19:19:14 -0300

    Implement update and delete operations

--- backend/db.py
+++ backend/db.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Any, List, Optional, Dict
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Article, Base
@@ -9,24 +9,42 @@ class ArticleDB:
 
     def __init__(self, uri: str) -> None:
         self.engine = create_engine(uri, echo=True, future=True)
-        self.session = sessionmaker(self.engine)
+        self.session = sessionmaker(self.engine)()
 
     def find_all(self) -> List[Article]:
-        with self.session() as session:
+        with self.session as session:
             list = session.query(Article).all()
             return list
 
     def find_by_id(self, id: int) -> Optional[Article]:
-        with self.session() as session:
+        with self.session as session:
             article = session.get(Article, ident=id)
             return article
 
-    def insert_article(self, article: Article) -> Article:
-        with self.session() as session:
+    def insert(self, article: Article) -> Article:
+        with self.session as session:
             session.add(article)
             session.flush()
             session.expunge(article)
             session.commit()
+            return article
+
+    def delete(self, id: int) -> bool:
+        with self.session as session:
+            article = session.get(Article, ident=id)
+            if not article:
+                return False
+            session.flush()
+            session.delete(article)
+            session.commit()
+            return True
+
+    def update(self, id: int, data: Dict[str, Any]) -> Article:
+        with self.session as session:
+            article = session.get(Article, ident=id)
+            session.query(Article).filter(Article.id == id).update(data)
+            session.flush()
+            session.expunge(article)
             return article
 
 
