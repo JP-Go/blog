29ac8343559f07284ba29e8e1aa5fd41a9f63ae5
João Pedro <jpedro1580@gmail.com>
2022-08-16 01:08:15 -0300

    Refactor tests, test expectations for findAll and findById. Add todos

--- backend/src/tests/articleService.spec.ts
+++ backend/src/tests/articleService.spec.ts
@@ -1,50 +1,30 @@
 import { InMemoryArticleRepository } from './stubs/InMemoryArticleRepository';
 import { ArticleService } from '../service/articleService';
-import { Article, ArticleInputProps, ArticleProps } from '../entity/Article';
+import { Article, ArticleInputProps } from '../entity/Article';
 
-describe('articleService tests', () => {
-  let ut: ArticleService;
-  let inputPropsStub: ArticleInputProps;
-  const now = new Date();
+const inputPropsStub: ArticleInputProps = {
+  title: 'test title',
+  body: 'The body of the test article',
+  authorName: 'Test',
+};
+const now = new Date();
 
-  beforeEach(() => {
-    ut = new ArticleService(new InMemoryArticleRepository());
-  });
+describe('ArticleService tests: An ArticleService', () => {
+  let sut: ArticleService;
 
   beforeAll(() => {
-    inputPropsStub = {
-      title: 'test title',
-      body: 'The body of the test article',
-      authorName: 'Test',
-    };
+    sut = new ArticleService(new InMemoryArticleRepository());
   });
 
-  it('should be able to get all articles', () => {
-    expect(ut.findAll()).toBeTruthy();
-    expect(ut.findAll()).toHaveLength(0);
-  });
-
-  describe('should be able to save new articles if provided the right parameters', () => {
+  describe('should be able to save articles', () => {
     it('shoud be able to save one article with the right data', () => {
-      const article = ut.save(inputPropsStub);
+      const article = sut.save(inputPropsStub);
       expect(article).toBeInstanceOf(Article);
       expect(article.id).toBe(1);
       expect(article).toHaveProperty('props');
       expect(article.props.created >= now).toBe(true);
       expect(article.props.lastUpdate >= now).toBe(true);
-      expect(ut.findAll()).toHaveLength(1);
-    });
-
-    it('should be able to save multiple articles', () => {
-      const articles = new Array(4).fill(0).map(() => ut.save(inputPropsStub));
-      const ids = articles.map((e) => e.id);
-      expect(articles.filter((e) => e instanceof Article)).toHaveLength(4);
-      expect(ids.every((e) => typeof e === 'number')).toBe(true);
-      expect(
-        ids.map((e, idx) => ids.indexOf(e) === idx).every((e) => e === true)
-      ).toBe(true);
-
-      expect(articles.every((a) => a.props !== undefined)).toBe(true);
+      expect(sut.findAll()).toHaveLength(1);
     });
 
     it('should throw if invalid paramaters are passed', () => {
@@ -56,19 +36,44 @@ describe('articleService tests', () => {
         age: 12,
       };
       expect(() => {
-        ut.save(notEnoughArguments);
+        sut.save(notEnoughArguments);
       }).toThrow();
       expect(() => {
-        ut.save(exaustiveArguments);
+        sut.save(exaustiveArguments);
       }).toThrow();
       expect(() => {
-        ut.save(invalidArguments);
+        sut.save(invalidArguments);
       }).toThrow();
     });
   });
 
-  describe('Should be able to get articles by id', () => {
-    it.todo('should get the article with corresponding id if present');
-    it.todo('should throw error if article if not found independent of reason');
+  describe('Should be able to get articles', () => {
+    it('should be able to return a collection of articles', () => {
+      expect(sut.findAll()).toBeInstanceOf(Array<Article>);
+    });
+
+    it('should get the article with corresponding id if present', () => {
+      const article = sut.findById(1);
+      expect(article).toBeInstanceOf(Article);
+      expect(article).toHaveProperty('id', 1);
+      expect(article).toHaveProperty('props');
+      expect(article?.props).toBeTruthy();
+    });
+
+    it('should return null if article was not found', () => {
+      const sut = new ArticleService(new InMemoryArticleRepository());
+      expect(sut.findById(1)).not.toBeNull();
+      expect(sut.findById(2)).toBeNull();
+    });
+  });
+
+  describe('Shoud be able to update articles', () => {
+    it.todo('shoud update the properties of the article');
+    it.todo('should not be able to update the articles id');
+  });
+
+  describe('Shoud be able to delete articles', () => {
+    it.todo('should delete article if present');
+    it.todo('should throw if article is not found');
   });
 });
