0025721492d6743c6ec6b80092382a85bdaf2e26
João Pedro <jpedro1580@gmail.com>
2022-08-17 12:22:45 -0300

    Fix cors error and InMemoryArticleRepository wierd state

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 import articles from './routes/articles';
 const app = express();
 const port = 5000;
 
 app.get('/', (_, res) => {
-    res.send('<h1>Hello</h1>');
+  res.send('<h1>Hello</h1>');
 });
+
+app.use(express.json());
+app.use(cors());
 
 app.use('/articles', articles);
 
 app.listen(port, () => {
-    console.log(
-        `Faça requisições em http://localhost:${port} para acessar o servidor`
-    );
+  console.log(
+    `Faça requisições em http://localhost:${port} para acessar o servidor`
+  );
 });
--- backend/src/tests/stubs/InMemoryArticleRepository.ts
+++ backend/src/tests/stubs/InMemoryArticleRepository.ts
@@ -1,19 +1,18 @@
 import { Article, ArticleProps } from '../../entity/Article';
 import { IRepository } from '../../repository/IRepository';
 
-let idCounter = 0;
-const articles: Article[] = [];
-
 export class InMemoryArticleRepository implements IRepository<Article> {
+  private idCounter = 0;
+  private articles: Article[] = [];
   save(props: ArticleProps): Article {
-    const newArticle = Article.create(props, ++idCounter);
-    articles.push(newArticle);
+    const newArticle = Article.create(props, ++this.idCounter);
+    this.articles.push(newArticle);
     return newArticle;
   }
   findById(id: number): Article | null {
-    return articles.find((a) => a.id === id) ?? null;
+    return this.articles.find((a) => a.id === id) ?? null;
   }
   findAll(): Article[] {
-    return articles;
+    return this.articles;
   }
 }
